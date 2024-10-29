455f1c0c54c5467ad69ff86d3681b009ff321f1f
i-am-wololo <somedudz@protonmail.com>
2024-10-29 18:58:10 +0100

    added session management

--- lua/plugins/lsps.lua
+++ lua/plugins/lsps.lua
@@ -9,6 +9,7 @@ local servers = {
 	"rust_analyzer",
 	"tinymist",
 	"mesonlsp",
+	"clangd"
 
 }
 
--- lua/plugins/misc.lua
+++ lua/plugins/misc.lua
@@ -1,9 +1,19 @@
 return {
-	{"andweeb/presence.nvim", lazy = false, opts = {}, config = function() require("presence").setup() end},
+	{
+		"andweeb/presence.nvim",
+		lazy = false,
+		opts = {
+			main_image = "file"
+
+		},
+	},
+
 	{
 		"nanotee/zoxide.vim",
 		lazy = false,
+
 	},
+
 	{
 	  "j-hui/fidget.nvim",
 	  opts = {
